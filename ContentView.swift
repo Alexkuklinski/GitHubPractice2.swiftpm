345ee702fc43e038aa1a3ffb49b34a5e462ea2cb
Alexander Kuklinski <akuklinski7210@stu.d214.org>
2025-04-11 13:03:15 -0500

    I Added textfueld connected to number2

--- ContentView.swift
+++ ContentView.swift
@@ -1,14 +1,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var number1 = 10
+    @State var number1 = 0
     @State var number2: Int = 0
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+        TextField("Enter Number", value: $number2, format: .number)
     }
 }
