c0903d1ea43ee6017cf62a5f3e47b6d42a9f03b1
Arcbuddy <jacobcaulfield10@gmail.com>
2025-04-11 12:59:25 -0500

    I removed the VStack and its contents and added a textfield connected to var number2

--- ContentView.swift
+++ ContentView.swift
@@ -4,11 +4,6 @@ struct ContentView: View {
     @State var number1 = 10
     @State var number2: Int = 0
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+        TextField("Enter a number", value: $number2, formatter: NumberFormatter())
     }
 }
