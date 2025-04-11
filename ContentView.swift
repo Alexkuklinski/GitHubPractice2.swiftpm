f6f3d8234818396b8e3f1079f6337258d664db1e
Arcbuddy <jacobcaulfield10@gmail.com>
2025-04-11 13:07:25 -0500

    I added a button that puts the some of number1 and number2 into another variable answer

--- ContentView.swift
+++ ContentView.swift
@@ -3,8 +3,13 @@ import SwiftUI
 struct ContentView: View {
     @State var number1 = 0
     @State var number2: Int = 0
+    @State var answer: Int = 0
     var body: some View {
         TextField("Enter Number", value: $number2, format: .number)
         TextField("Enter a number", value: $number1, formatter: NumberFormatter())
+        Divider()
+        Button("Add"){
+            answer = number1 + number2
+        }
     }
 }
