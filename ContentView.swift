a8a28ce14355eadbe44b9737fdd90225b61c9c90
Arcbuddy <jacobcaulfield10@gmail.com>
2025-04-11 13:04:29 -0500

    Changed the variable

--- ContentView.swift
+++ ContentView.swift
@@ -4,6 +4,6 @@ struct ContentView: View {
     @State var number1 = 10
     @State var number2: Int = 0
     var body: some View {
-        TextField("Enter a number", value: $number2, formatter: NumberFormatter())
+        TextField("Enter a number", value: $number1, formatter: NumberFormatter())
     }
 }
